cc1dbf268a3672a503dc1e7f199093662d665925
Davide Lugli <luglidavide.web@gmail.com>
2020-01-13 18:25:04 +0100

    palindroma fix

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,27 +3,43 @@
 var parola = prompt('inserisci una parola');
 console.log(parola);
 
+var parolaGirata = rovesciaParola(parola);
 
-function palindromo(parola) {
-  // Uso split() per un nuovo array
-  var splitParola = parola.split("");
-  console.log(splitParola);
-  // reverse() per creare l'opposto del nuovo array
-  var reverseParola = splitParola.reverse();
-  console.log(reverseParola);
-  //  join() per unire gli elementi dell'array in un unica stringa
-  var joinArray = reverseParola.join("");
-  // return la nuova stringa
-  return joinArray;
-}
-
-console.log(palindromo(parola));
-
-var controlloParola = false;
-
-if (parola == palindromo(parola)) {
-  controlloParola = true;
-  console.log(controlloParola + ' ' + parola + ' ' + 'è una parola palindroma');
+if (parola == parolaGirata) {
+  console.log(parola + ' ' + 'è una parola palindroma');
 } else {
-  console.log(controlloParola + ' ' + parola + ' ' + ' non è una parola palindroma');
+  console.log(parola + ' ' + 'non è una parola palindroma');
 }
+
+function rovesciaParola(parolaUtente) {
+  var parolaRovesciata = '';
+  for (var i = parolaUtente.length - 1; i >= 0; i--) {
+    parolaRovesciata += parolaUtente[i];
+  }
+  return parolaRovesciata;
+}
+
+// con function reverse
+// function palindromo(parola) {
+//   // Uso split() per un nuovo array
+//   var splitParola = parola.split("");
+//   console.log(splitParola);
+//   // reverse() per creare l'opposto del nuovo array
+//   var reverseParola = splitParola.reverse();
+//   console.log(reverseParola);
+//   //  join() per unire gli elementi dell'array in un unica stringa
+//   var joinArray = reverseParola.join("");
+//   // return la nuova stringa
+//   return joinArray;
+// }
+//
+// console.log(palindromo(parola));
+//
+// var controlloParola = false;
+//
+// if (parola == palindromo(parola)) {
+//   controlloParola = true;
+//   console.log(controlloParola + ' ' + parola + ' ' + 'è una parola palindroma');
+// } else {
+//   console.log(controlloParola + ' ' + parola + ' ' + ' non è una parola palindroma');
+// }
